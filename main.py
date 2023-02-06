6caafa2dce6f94a8c7906d5489224067833cc51c
bekkkks <ewefe811@gmail.com>
2023-02-06 19:20:55 +0600

    'beka'

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # This is a sample Python script.
+import decouple
+
+TOKEN = ''
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+p = ''
 
 
 def print_hi(name):
@@ -14,9 +19,17 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-class A1:
-    def __init__(self,a):
-        self.a=a
+import random
 
-a=A1('a')
+
+class A1:
+    def __init__(self, a):
+        self.a = a
+
+
+a = A1('a')
 print(a)
+import colorama
+print(colorama.Back.GREEN)
+print(colorama.Back.GREEN)
+print(colorama.Back.GREEN)
